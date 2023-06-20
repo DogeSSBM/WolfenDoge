99cb76494bccd24be65ca0d93817415081d7fde6
dogeThinkpad <dogessbm@gmail.com>
2023-06-20 05:32:46 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include "DogeLib/Includes.h"
-#define WALLS 8
-#define BOUNDS 750.0f
+Coordf fC(const float x, const float y)
+{
+    return (const Coordf){.x=x, .y=y};
+}
+
+float fmost(const float a, const float b)
+{
+    return a > b ? a : b;
+}
+
+float cfMax(const Coordf pos)
+{
+    return fmost(pos.x, pos.y);
+}
 
 typedef struct{
     Coord pos;
@@ -17,7 +29,14 @@ typedef struct Wall{
     Color c;
     Coordf a;
     Coordf b;
+    struct Wall *next;
 }Wall;
+
+typedef struct{
+    Wall *wall;
+    float dst;
+    Coordf pos;
+}Ray;
 
 Coord toView(const View view, const Coordf pos, const float scale)
 {
@@ -29,8 +48,11 @@ bool lineIntersection(const Coordf p0, const Coordf p1, const Coordf p2, const C
     const Coordf s1 = {.x = p1.x - p0.x, .y = p1.y - p0.y};
     const Coordf s2 = {.x = p3.x - p2.x, .y = p3.y - p2.y};
 
-    const float s = (-s1.y * (p0.x - p2.x) + s1.x * (p0.y - p2.y)) / (-s2.x * s1.y + s1.x * s2.y);
-    const float t = ( s2.x * (p0.y - p2.y) - s2.y * (p0.x - p2.x)) / (-s2.x * s1.y + s1.x * s2.y);
+
+    const float d = -s2.x * s1.y + s1.x * s2.y;
+
+    const float s = d!=0 ? (-s1.y * (p0.x - p2.x) + s1.x * (p0.y - p2.y)) / d : 1000000.0f;
+    const float t = d!=0 ? ( s2.x * (p0.y - p2.y) - s2.y * (p0.x - p2.x)) / d : 1000000.0f;
 
     if (s >= 0 && s <= 1 && t >= 0 && t <= 1){
         if(at){
@@ -42,7 +64,30 @@ bool lineIntersection(const Coordf p0, const Coordf p1, const Coordf p2, const C
     return false;
 }
 
-void drawFp(const View view, const Wall map[WALLS], const Player player)
+Ray castRay(const Coordf origin, const Coordf distantPoint, Wall *map)
+{
+    Ray ray = {
+        .wall = map,
+        .dst = 6000.0f,
+        .pos = {.x = 6000.0f, .y = 6000.0f}
+    };
+    while(map){
+        float curDst = 0;
+        Coordf curPos = {0};
+        if(
+            lineIntersection(origin, distantPoint, map->a, map->b, &curPos) &&
+            (curDst = cfDist(origin, curPos)) < ray.dst
+        ){
+            ray.wall = map;
+            ray.dst = curDst;
+            ray.pos = curPos;
+        }
+        map = map->next;
+    }
+    return ray;
+}
+
+void drawFp(const View view, Wall *map, const Player player)
 {
     setColor(GREY2);
     fillRectCoordLength(view.pos, iC(view.len.x,view.len.y/2));
@@ -54,71 +99,43 @@ void drawFp(const View view, const Wall map[WALLS], const Player player)
 
     const float hsec = (float)view.len.x/90;
     for(int i = 0; i < 90; i++){
-        float dst = 6000.0f;
-        Coordf pos = cfAdd(startingPos, degMagToCf(scanAng, ((float)i/90.0f)*2048.0f));
-        Color c = {0};
-        for(int w = 0; w < WALLS; w++){
-            float cur = 0;
-            if(
-                lineIntersection(player.pos, pos, map[w].a, map[w].b, &pos) &&
-                (cur = cfDist(player.pos, pos)) < dst
-            ){
-                dst = cur;
-                c = map[w].c;
-            }
-        }
+        Ray ray = castRay(player.pos, cfAdd(startingPos, degMagToCf(scanAng, ((float)i/90.0f)*2048.0f)), map);
         const float viewTan = (0.5-i/90.0f) / 0.5;
-        const int correctedDst = (int)(dst/sqrtf(viewTan*viewTan+1.0f));
+        const int correctedDst = (int)(ray.dst/sqrtf(viewTan*viewTan+1.0f));
+        Color c = ray.wall->c;
         c.r = clamp(c.r-((correctedDst/1000.0f)*255), 0, 256);
         c.g = clamp(c.g-((correctedDst/1000.0f)*255), 0, 256);
         c.b = clamp(c.b-((correctedDst/1000.0f)*255), 0, 256);
         setColor(c);
         fillRectCenteredCoordLength(
             iC(view.pos.x+hsec/2+i*hsec, view.pos.y+view.len.y/2),
-            iC(hsec+1, (view.len.y*120) / correctedDst)
+            iC(hsec+1, imin(view.len.y, (view.len.y*120) / (correctedDst ? correctedDst : .01f)))
         );
     }
 }
 
-void drawBv(const View view, const Wall map[WALLS], const Player player)
+void drawBv(const View view, Wall *map, const Player player, const float scale, const Coordf off)
 {
     setColor(BLACK);
     fillRectCoordLength(view.pos, view.len);
-    const float scale = (float)coordMin(view.len) / BOUNDS;
+    (void)off;
 
-    for(uint i = 0; i < WALLS; i++){
-        const Coord a = toView(view, map[i].a, scale);
-        const Coord b = toView(view, map[i].b, scale);
-        setColor(map[i].c);
+    Wall *cur = map;
+    while(cur){
+        const Coord a = toView(view, cur->a, scale);
+        const Coord b = toView(view, cur->b, scale);
+        setColor(cur->c);
         drawLineCoords(a, b);
+        cur = cur->next;
     }
-    const Coordf posl = cfAdd(player.pos, degMagToCf(degReduce(player.ang - 45.0f), 1000.0f));
-    const Coordf posr = cfAdd(player.pos, degMagToCf(degReduce(player.ang + 45.0f), 1000.0f));
-    Coordf minl = {.x=1000.0f, .y=0};
-    float dstl = 1000.0f;
-    Coordf minr = {.x=1000.0f, .y=0};
-    float dstr = 1000.0f;
 
-    for(uint i = 0; i < WALLS; i++){
-        Coordf pos = {0};
-        float dst = 0;
-        if(lineIntersection(player.pos, posl, map[i].a, map[i].b, &pos)){
-            if((dst = cfDist(player.pos, pos)) < dstl){
-                dstl = dst;
-                minl = pos;
-            }
-        }
-        if(lineIntersection(player.pos, posr, map[i].a, map[i].b, &pos)){
-            if((dst = cfDist(player.pos, pos)) < dstr){
-                dstr = dst;
-                minr = pos;
-            }
-        }
-    }
+    Ray rayl = castRay(player.pos, cfAdd(player.pos, degMagToCf(degReduce(player.ang - 45.0f), 1000.0f)), map);
+    Ray rayr = castRay(player.pos, cfAdd(player.pos, degMagToCf(degReduce(player.ang + 45.0f), 1000.0f)), map);
+
     const Coord ppos = toView(view, player.pos, scale);
     setColor(YELLOW);
-    drawLineCoords(ppos, toView(view, minr, scale));
-    drawLineCoords(ppos, toView(view, minl, scale));
+    drawLineCoords(ppos, toView(view, rayr.pos, scale));
+    drawLineCoords(ppos, toView(view, rayl.pos, scale));
     fillCircleCoord(ppos, 2);
 }
 
@@ -144,25 +161,88 @@ Player playerMoveKeys(Player player)
     return player;
 }
 
+Wall* wallNew(const Color c, const Coordf a, const Coordf b)
+{
+    Wall *w = calloc(1, sizeof(Wall));
+    w->c = c;
+    w->a = a;
+    w->b = b;
+    return w;
+}
+
+Wall* wallAppend(Wall *head, Wall *tail)
+{
+    if(!head)
+        return tail;
+    Wall *cur = head;
+    while(cur->next)
+        cur = cur->next;
+    cur->next = tail;
+    return head;
+}
+
+Coordf mapBounds(Wall *map)
+{
+    Coordf bound = {0};
+    while(map)
+    {
+        bound.x = fmost(bound.x, map->a.x);
+        bound.x = fmost(bound.x, map->b.x);
+        bound.y = fmost(bound.y, map->a.y);
+        bound.y = fmost(bound.y, map->b.y);
+        map = map->next;
+    }
+    return bound;
+}
+
+void editMap(Wall *map)
+{
+    Player player = {.pos = {.x=125.0f, .y=125.0f}};
+    Coordf off = fC(0,0);
+    SDL_SetRelativeMouseMode(false);
+
+    while(1){
+        const uint t = frameStart();
+
+        const Length wlen = getWindowLen();
+        const View birdsView = {.len = wlen};
+        const View firstView = {.len = coordDivi(wlen, 4), .pos = {.x=wlen.x-wlen.x/4}};
+        const Coordf bound = mapBounds(map);
+        float scale = (float)coordMin(birdsView.len) / cfMax(bound);
+        if(keyPressed(SDL_SCANCODE_ESCAPE))
+            return;
+
+        player = playerMoveKeys(player);
+
+        if(mouseBtnState(MOUSE_M))
+            off = cfAdd(off, cfMulf(CCf(mouseMovement()), scale));
+        // Coordf mouseMapPos = toView()
+
+        drawBv(birdsView, map, player, scale, off);
+        drawFp(firstView, map, player);
+
+        frameEnd(t);
+    }
+}
+
 int main(void)
 {
     init();
     gfx.outlined = false;
     winSetPosCoord(coordAddi(coordDivi(getWinDisplayLen(), 2), -400));
     setColor(WHITE);
+    Wall *map =           wallNew(GREEN,    (const Coordf){.x=  0.0f, .y=  0.0f},   (const Coordf){.x=750.0f, .y=  0.0f});
+    map = wallAppend(map, wallNew(MAGENTA,  (const Coordf){.x=750.0f, .y=  0.0f},   (const Coordf){.x=750.0f, .y=750.0f}));
+    map = wallAppend(map, wallNew(MAGENTA,  (const Coordf){.x=  0.0f, .y=  0.0f},   (const Coordf){.x=  0.0f, .y=750.0f}));
+    map = wallAppend(map, wallNew(GREEN,    (const Coordf){.x=  0.0f, .y=750.0f},   (const Coordf){.x=750.0f, .y=750.0f}));
+    map = wallAppend(map, wallNew(BLUE,     (const Coordf){.x=250.0f, .y=250.0f},   (const Coordf){.x=500.0f, .y=250.0f}));
+    map = wallAppend(map, wallNew(BLUE,     (const Coordf){.x=500.0f, .y=250.0f},   (const Coordf){.x=500.0f, .y=500.0f}));
+    map = wallAppend(map, wallNew(BLUE,     (const Coordf){.x=250.0f, .y=250.0f},   (const Coordf){.x=250.0f, .y=500.0f}));
+    map = wallAppend(map, wallNew(BLUE,     (const Coordf){.x=250.0f, .y=500.0f},   (const Coordf){.x=500.0f, .y=500.0f}));
+
+    editMap(map);
     SDL_SetRelativeMouseMode(true);
-    const Wall map[WALLS] = {
-        { .c = GREEN,   .a={.x=  0.0f, .y=  0.0f}, .b={.x=750.0f, .y=  0.0f} },
-        { .c = MAGENTA, .a={.x=750.0f, .y=  0.0f}, .b={.x=750.0f, .y=750.0f} },
-        { .c = MAGENTA, .a={.x=  0.0f, .y=  0.0f}, .b={.x=  0.0f, .y=750.0f} },
-        { .c = GREEN,   .a={.x=  0.0f, .y=750.0f}, .b={.x=750.0f, .y=750.0f} },
-
-        { .c = BLUE,    .a={.x=250.0f, .y=250.0f}, .b={.x=500.0f, .y=250.0f} },
-        { .c = BLUE,    .a={.x=500.0f, .y=250.0f}, .b={.x=500.0f, .y=500.0f} },
-        { .c = BLUE,    .a={.x=250.0f, .y=250.0f}, .b={.x=250.0f, .y=500.0f} },
-        { .c = BLUE,    .a={.x=250.0f, .y=500.0f}, .b={.x=500.0f, .y=500.0f} }
-    };
-
+    
     Player player = {.pos = {.x=125.0f, .y=125.0f}};
 
     while(1){
@@ -172,10 +252,13 @@ int main(void)
             return 0;
 
         const Length wlen = getWindowLen();
+        const View firstView = {.len = wlen};
+        const View birdsView = {.len = iC(coordMin(wlen)/4,coordMin(wlen)/4), .pos = {.x=wlen.x-coordMin(wlen)/4}};
+
         player = playerMoveMouse(player);
 
-        drawFp((View){.len = wlen}, map, player);
-        drawBv((View){.len = coordDivi(wlen, 4), .pos = {.x=wlen.x/4*3}}, map, player);
+        drawFp(firstView, map, player);
+        drawBv(birdsView, map, player, coordMin(birdsView.len)/ cfMax(mapBounds(map)), fC(0,0));
 
         frameEnd(t);
     }
