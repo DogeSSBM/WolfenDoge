dd15d058f68fe2aae48a38b0181fce2883e37085
dogeThinkpad <dogessbm@gmail.com>
2023-06-23 16:54:19 -0400

    Automatic commit.

--- Map.h
+++ Map.h
@@ -1,6 +1,358 @@
 #ifndef MAP_H
 #define MAP_H
 
+WallPacked* mapPack(Wall *map)
+{
+    const uint len = wallListLen(map);
+    if(len == 0)
+        return NULL;
+    WallPacked *mapPacked = calloc(len, sizeof(WallPacked));
+    for(uint i = 0; i < len; i++){
+        mapPacked[i].c = map->c;
+        mapPacked[i].a = map->a;
+        mapPacked[i].b = map->b;
+        map = map->next;
+    }
+    return mapPacked;
+}
 
+Wall* mapUnpack(WallPacked *mapPacked, const uint len)
+{
+    if(len == 0)
+        return NULL;
+    Wall *map = NULL;
+    for(uint i = 0; i < len; i++)
+        map = wallAppend(map, wallNew(mapPacked[i].c, mapPacked[i].a, mapPacked[i].b));
+    return map;
+}
+
+Coordf mapBounds(Wall *map)
+{
+    Coordf bound = {0};
+    while(map)
+    {
+        bound.x = fmost(bound.x, map->a.x);
+        bound.x = fmost(bound.x, map->b.x);
+        bound.y = fmost(bound.y, map->a.y);
+        bound.y = fmost(bound.y, map->b.y);
+        map = map->next;
+    }
+    return bound;
+}
+
+Coord resizeTransform(const Length oldLen, const Length newLen, const Coord pos)
+{
+    return CfC(cfMul(CCf(newLen), cfDiv(CCf(pos), CCf(oldLen))));
+}
+
+Coordf resizeTransformf(const Lengthf oldLen, const Lengthf newLen, const Coordf pos)
+{
+    return cfMul(newLen, cfDiv(pos, oldLen));
+}
+
+Coordf screenToMap(const Coord off, const float scale, const Coord pos)
+{
+    return cfMulf(CCf(coordSub(pos, off)), scale);
+}
+
+Coord mapToScreen(const Coord off, const float scale, const Coordf pos)
+{
+    return coordAdd(CfC(cfDivf(pos, scale)), off);
+}
+
+Coordf absOff(const Coord len, const Coord pos)
+{
+    return cfDiv(CCf(pos), CCf(len));
+}
+
+Wall* posNearest(Wall *map, const Coordf pos, Coordf **nearest)
+{
+    if(!map){
+        nearest = NULL;
+        return NULL;
+    }
+    Wall *wall = map;
+    *nearest = &(map->a);
+    float dst = cfDist(pos, map->a);
+    while(map){
+        float curDst = cfDist(pos, map->a);
+        if(curDst < dst){
+            dst = curDst;
+            wall = map;
+            *nearest = &(map->a);
+        }
+        curDst = cfDist(pos, map->b);
+        if(curDst < dst){
+            dst = curDst;
+            wall = map;
+            *nearest = &(map->b);
+        }
+        map = map->next;
+    }
+    return wall;
+}
+
+void defaultMapFileName(char *fileName)
+{
+    uint i = 0;
+    File *file = NULL;
+    sprintf(fileName, "map.bork");
+    while((file = fopen(fileName, "rb")) != NULL){
+        fclose(file);
+        i++;
+        sprintf(fileName, "map(%u).bork", i);
+    }
+}
+
+void mapSave(Wall *map, char *fileName)
+{
+    if(!map){
+        printf("Map is empty, skipping save\n");
+        return;
+    }
+    File *file = NULL;
+    char defaultName[64] = {0};
+    if(!fileName){
+        defaultMapFileName(defaultName);
+        fileName = defaultName;
+    }
+    file = fopen(fileName, "wb");
+    const uint len = wallListLen(map);
+    WallPacked *mapPacked = mapPack(map);
+    fwrite(&len, sizeof(uint), 1, file);
+    fwrite(mapPacked, sizeof(WallPacked), len, file);
+    fclose(file);
+    free(mapPacked);
+}
+
+Wall* wallNew(const Color c, const Coordf a, const Coordf b)
+{
+    Wall *w = calloc(1, sizeof(Wall));
+    w->c = c;
+    w->a = a;
+    w->b = b;
+    return w;
+}
+
+Wall* wallAppend(Wall *head, Wall *tail)
+{
+    if(!head)
+        return tail;
+    Wall *cur = head;
+    while(cur->next)
+        cur = cur->next;
+    cur->next = tail;
+    return head;
+}
+
+Wall* wallDelete(Wall *map, Wall *del)
+{
+    if(!del)
+        return map;
+    if(!map)
+        return NULL;
+    if(del == map){
+        Wall *next = map->next;
+        free(map);
+        return next;
+    }
+    Wall *cur = map;
+    while(cur && cur->next != del)
+        cur = cur->next;
+    if(!cur)
+        return map;
+    Wall *next = cur->next->next;
+    free(cur->next);
+    cur->next = next;
+    return map;
+}
+
+uint wallListLen(Wall *map)
+{
+    uint len = 0;
+    while(map){
+        len++;
+        map = map->next;
+    }
+    return len;
+}
+
+void wallFreeList(Wall *list)
+{
+    while(list){
+        Wall *next = list->next;
+        free(list);
+        list = next;
+    }
+}
+
+Wall* mapDefault(void)
+{
+    Wall *map =           wallNew(GREEN,    (const Coordf){.x=  0.0f, .y=  0.0f},   (const Coordf){.x=750.0f, .y=  0.0f});
+    map = wallAppend(map, wallNew(MAGENTA,  (const Coordf){.x=750.0f, .y=  0.0f},   (const Coordf){.x=750.0f, .y=750.0f}));
+    map = wallAppend(map, wallNew(MAGENTA,  (const Coordf){.x=  0.0f, .y=  0.0f},   (const Coordf){.x=  0.0f, .y=750.0f}));
+    map = wallAppend(map, wallNew(GREEN,    (const Coordf){.x=  0.0f, .y=750.0f},   (const Coordf){.x=750.0f, .y=750.0f}));
+    map = wallAppend(map, wallNew(BLUE,     (const Coordf){.x=250.0f, .y=250.0f},   (const Coordf){.x=500.0f, .y=250.0f}));
+    map = wallAppend(map, wallNew(BLUE,     (const Coordf){.x=500.0f, .y=250.0f},   (const Coordf){.x=500.0f, .y=500.0f}));
+    map = wallAppend(map, wallNew(BLUE,     (const Coordf){.x=250.0f, .y=250.0f},   (const Coordf){.x=250.0f, .y=500.0f}));
+    map = wallAppend(map, wallNew(BLUE,     (const Coordf){.x=250.0f, .y=500.0f},   (const Coordf){.x=500.0f, .y=500.0f}));
+    return map;
+}
+
+Wall* mapEdit(Wall *map, char *fileName)
+{
+    float scale = 1.0f;
+    bool snap = true;
+    uint snaplen = 24;
+    Coord off = {0};
+    SDL_SetRelativeMouseMode(false);
+    Length wlen = getWindowLen();
+    bool rdrag = false;
+    Coord mrd = {0};
+    Coord mld = {0};
+    bool ldrag = false;
+
+    Wall *selectedWall = NULL;
+    Coordf *selectedPos = NULL;
+
+    Color c = MAGENTA;
+    int ci = 0;
+    while(1){
+        const uint t = frameStart();
+        if(checkCtrlKey(SDL_SCANCODE_Q) || checkCtrlKey(SDL_SCANCODE_W)){
+            return map;
+        }
+
+        if(checkCtrlKey(SDL_SCANCODE_S)){
+            mapSave(map, fileName);
+            return map;
+        }else if(keyPressed(SDL_SCANCODE_S)){
+            snap = !snap;
+        }
+
+        ci = wrap(ci + keyPressed(SDL_SCANCODE_RIGHT) - keyPressed(SDL_SCANCODE_LEFT), 0, 3);
+        u8* b = colorIndex(&c, ci);
+        int change = (keyPressed(SDL_SCANCODE_UP) - keyPressed(SDL_SCANCODE_DOWN));
+        change *= (1+(keyState(SDL_SCANCODE_LCTRL) || keyState(SDL_SCANCODE_RCTRL)))*10;
+        *b = clamp((int)(*b) + change, 0, 255);
+
+        if(keyPressed(SDL_SCANCODE_ESCAPE)){
+            selectedWall = NULL;
+            selectedPos = NULL;
+            rdrag = false;
+            ldrag = false;
+        }
+
+        if(windowResized()){
+            const Length wlenOld = wlen;
+            wlen = getWindowLen();
+            off = resizeTransform(wlenOld, wlen, off);
+        }
+
+        if((keyPressed(SDL_SCANCODE_DELETE) || keyPressed(SDL_SCANCODE_BACKSPACE)) && selectedWall){
+            map = wallDelete(map, selectedWall);
+            selectedWall = NULL;
+            selectedPos = NULL;
+        }
+
+        if(mouseScrolledY()){
+            if(keyState(SDL_SCANCODE_LCTRL) || keyState(SDL_SCANCODE_RCTRL)){
+                snaplen = imax(1, snaplen + mouseScrolledY());
+            }else{
+                const float oldScale = scale;
+                scale = fclamp(scale * (mouseScrolledY() > 0 ? 1.2f : .8f) , .01f, 100.0f);
+                if(oldScale != scale){
+                    const Coordf mopos = screenToMap(off, oldScale, coordDivi(wlen,2));
+                    const Coordf mpos = screenToMap(off, scale, coordDivi(wlen,2));
+                    const Coord dif = CfC(cfSub(mpos, mopos));
+                    off = coordAdd(off, coordDivi(dif, 2));
+                    // const Coordf offMag = cfAddf(cfDiv(CCf(mouse.pos), CCf(wlen)), -.05f);
+                    // off = mapToScreen(off, scale, cfMul(cfSub(mpos, mopos), offMag));
+
+
+                }
+            }
+        }
+
+        if(mouseBtnState(MOUSE_M) || keyState(SDL_SCANCODE_LSHIFT)){
+            off = coordAdd(off, mouseMovement());
+            mrd = coordAdd(mrd, mouseMovement());
+            mld = coordAdd(mld, mouseMovement());
+        }
+
+        if(!selectedWall && mouseBtnReleased(MOUSE_L)){
+            selectedWall = posNearest(map, screenToMap(off, scale, mouse.pos), &selectedPos);
+        }
+
+        if(mouseBtnPressed(MOUSE_L) && selectedPos){
+            ldrag = true;
+            mld = mouse.pos;
+        }
+
+        if(ldrag && mouseBtnReleased(MOUSE_L)){
+            ldrag = false;
+        }
+
+        if(ldrag && selectedPos){
+            if(snap){
+                *selectedPos = cfAdd(*selectedPos, screenToMap(off,scale, mouseMovement()));
+            }else{
+                *selectedPos = cfAdd(*selectedPos, screenToMap(off,scale, mouseMovement()));
+            }
+        }
+
+        if(mouseBtnPressed(MOUSE_R)){
+            mrd = mouse.pos;
+            rdrag = true;
+        }
+
+        if(rdrag && mouseBtnReleased(MOUSE_R)){
+            rdrag = false;
+            map = wallAppend(map, wallNew(c, screenToMap(off, scale, mrd), screenToMap(off, scale, mouse.pos)));
+        }
+
+        if(rdrag){
+            setColor(c);
+            drawLineCoords(mrd, mouse.pos);
+            setColor(YELLOW);
+            fillCircleCoord(mouse.pos, 8);
+            setColor(GREEN);
+            fillCircleCoord(mrd, 8);
+        }
+
+        Wall *cur = map;
+        while(cur){
+            const Coord a = mapToScreen(off, scale, cur->a);
+            const Coord b = mapToScreen(off, scale, cur->b);
+            setColor(cur->c);
+            drawLineCoords(a, b);
+            cur = cur->next;
+        }
+
+        setColor(RED);
+        drawHLine(0, off.y, wlen.x);
+        drawVLine(off.x, 0, wlen.y);
+
+        // if(snap){
+        //     const Coordf start = cfSnap(screenToMap(off, scale, off), scale);
+        //     Coord spos = mapToScreen(off, scale, start);
+        //     for(Coordf pos = start; spos.x < wlen.x || spos.y < wlen.y; cfAddf(pos, snap)){
+        //         spos = mapToScreen(off, scale, pos);
+        //         drawVLine(spos.x, 0, wlen.y);
+        //         drawHLine(0, spos.y, wlen.x);
+        //     }
+        // }
+
+        if(selectedPos)
+            fillCircleCoord(mapToScreen(off, scale, *selectedPos), 8);
+        if(selectedWall)
+            drawCircleCoord(mapToScreen(off, scale, &(selectedWall->a) == selectedPos ? selectedWall->b : selectedWall->a), 8);
+
+        setColor(c);
+        fillCircleCoord(wlen, 8);
+
+        frameEnd(t);
+    }
+}
 
 #endif /* end of include guard: MAP_H */
---
+++ WolfenDoge.h
@@ -0,0 +1,256 @@
+#ifndef WOLFENDOGE_H
+#define WOLFENDOGE_H
+
+typedef struct{
+    Coord pos;
+    Length len;
+}View;
+
+typedef struct Player{
+    Coordf pos;
+    float ang;
+    float speed;
+}Player;
+
+typedef struct Wall{
+    Color c;
+    Coordf a;
+    Coordf b;
+    struct Wall *next;
+}Wall;
+
+typedef struct{
+    Wall *wall;
+    float dst;
+    Coordf pos;
+}Ray;
+
+typedef struct{
+    Color c;
+    Coordf a;
+    Coordf b;
+}WallPacked;
+
+#include "Decls.h"
+#include "Map.h"
+
+Coordf fC(const float x, const float y)
+{
+    return (const Coordf){.x=x, .y=y};
+}
+
+float fmost(const float a, const float b)
+{
+    return a > b ? a : b;
+}
+
+float cfMax(const Coordf pos)
+{
+    return fmost(pos.x, pos.y);
+}
+
+bool cfSame(const Coordf a, const Coordf b)
+{
+    return a.x == b.x && a.y == b.y;
+}
+
+Coordf cfMod(const Coordf pos, const float mod)
+{
+    return (const Coordf){
+        .x = fmod(pos.x, mod),
+        .y = fmod(pos.y, mod)
+    };
+}
+
+Coordf cfSub(const Coordf a, const Coordf b)
+{
+    return (const Coordf){
+        .x = a.x-b.x,
+        .y = a.y-b.y
+    };
+}
+
+Coordf cfAddf(const Coordf pos, const float f)
+{
+    return (const Coordf){
+        .x=pos.x+f,
+        .y=pos.y+f
+    };
+}
+
+Coordf cfSnap(const Coordf pos, const float scale)
+{
+    return cfSub(pos, cfMod(pos, scale));
+}
+
+Coord coordAbs(const Coord pos)
+{
+    return (const Coord){.x=pos.x<0?-pos.x:pos.x, .y=pos.y<0?-pos.y:pos.y};
+}
+
+u8* colorIndex(Color *c, const int i)
+{
+    if(iabs(i)%3 == 0)
+        return &(c->r);
+    if(iabs(i)%3 == 1)
+        return &(c->g);
+    return &(c->b);
+}
+
+bool checkCtrlKey(const Scancode key)
+{
+    return keyPressed(key) && (keyState(SDL_SCANCODE_LCTRL) || keyState(SDL_SCANCODE_RCTRL));
+}
+
+Coord toView(const View view, const Coordf pos, const float scale)
+{
+    return coordAdd(view.pos, iC(pos.x*scale, pos.y*scale));
+}
+
+bool lineIntersection(const Coordf p0, const Coordf p1, const Coordf p2, const Coordf p3, Coordf *at)
+{
+    const Coordf s1 = {.x = p1.x - p0.x, .y = p1.y - p0.y};
+    const Coordf s2 = {.x = p3.x - p2.x, .y = p3.y - p2.y};
+
+
+    const float d = -s2.x * s1.y + s1.x * s2.y;
+
+    const float s = d!=0 ? (-s1.y * (p0.x - p2.x) + s1.x * (p0.y - p2.y)) / d : 1000000.0f;
+    const float t = d!=0 ? ( s2.x * (p0.y - p2.y) - s2.y * (p0.x - p2.x)) / d : 1000000.0f;
+
+    if (s >= 0 && s <= 1 && t >= 0 && t <= 1){
+        if(at){
+            at->x = p0.x + (t * s1.x);
+            at->y = p0.y + (t * s1.y);
+        }
+        return true;
+    }
+    return false;
+}
+
+Ray castRay(const Coordf origin, const Coordf distantPoint, Wall *map)
+{
+    Ray ray = {
+        .wall = map,
+        .dst = 60000.0f,
+        .pos = {.x = 60000.0f, .y = 60000.0f}
+    };
+    while(map){
+        float curDst = 0;
+        Coordf curPos = {0};
+        if(
+            lineIntersection(origin, distantPoint, map->a, map->b, &curPos) &&
+            (curDst = cfDist(origin, curPos)) < ray.dst
+        ){
+            ray.wall = map;
+            ray.dst = curDst;
+            ray.pos = curPos;
+        }
+        map = map->next;
+    }
+    return ray;
+}
+
+void drawFp(const View view, Wall *map, const Player player)
+{
+    setColor(GREY2);
+    fillRectCoordLength(view.pos, iC(view.len.x,view.len.y/2));
+    setColor(GREY3);
+    fillRectCoordLength(iC(view.pos.x,view.len.y/2), iC(view.len.x,view.len.y/2));
+
+    const Coordf startingPos = cfAdd(player.pos, cfRotateDeg((const Coordf){.x=2048.0f,.y=-2048.0f}, player.ang));
+    const float scanAng = degReduce(player.ang+90.0f);
+
+    const float hsec = (float)view.len.x/90;
+    for(int i = 0; i < 90; i++){
+        Ray ray = castRay(player.pos, cfAdd(startingPos, degMagToCf(scanAng, ((float)i/90.0f)*4096.0f)), map);
+        const float viewTan = (0.5-i/90.0f) / 0.5;
+        const int correctedDst = (int)(ray.dst/sqrtf(viewTan*viewTan+1.0f));
+        if(ray.wall){
+            Color c = ray.wall->c;
+            c.r = clamp(c.r-(((correctedDst*1.2f)/2000.0f)*255), 0, 256);
+            c.g = clamp(c.g-(((correctedDst*1.2f)/2000.0f)*255), 0, 256);
+            c.b = clamp(c.b-(((correctedDst*1.2f)/2000.0f)*255), 0, 256);
+            setColor(c);
+        }else{
+            setColor(BLACK);
+        }
+        fillRectCenteredCoordLength(
+            iC(view.pos.x+hsec/2+i*hsec, view.pos.y+view.len.y/2),
+            iC(hsec+1, imin(view.len.y, (view.len.y*120) / (correctedDst ? correctedDst : .01f)))
+        );
+    }
+}
+
+void drawBv(const View view, Wall *map, const Player player, const float scale, const Coordf off)
+{
+    setColor(BLACK);
+    fillRectCoordLength(view.pos, view.len);
+    (void)off;
+
+    Wall *cur = map;
+    while(cur){
+        const Coord a = toView(view, cur->a, scale);
+        const Coord b = toView(view, cur->b, scale);
+        setColor(cur->c);
+        drawLineCoords(a, b);
+        cur = cur->next;
+    }
+
+    Ray rayl = castRay(player.pos, cfAdd(player.pos, degMagToCf(degReduce(player.ang - 45.0f), 1000.0f)), map);
+    Ray rayr = castRay(player.pos, cfAdd(player.pos, degMagToCf(degReduce(player.ang + 45.0f), 1000.0f)), map);
+
+    const Coord ppos = toView(view, player.pos, scale);
+    setColor(YELLOW);
+    drawLineCoords(ppos, toView(view, rayr.pos, scale));
+    drawLineCoords(ppos, toView(view, rayl.pos, scale));
+    fillCircleCoord(ppos, 2);
+}
+
+Player playerMoveMouse(Player player)
+{
+    const Scancode dirkey[4] = {SDL_SCANCODE_A, SDL_SCANCODE_W, SDL_SCANCODE_D, SDL_SCANCODE_S};
+    player.ang = degReduce(player.ang + (mouse.vec.x*2)/3);
+    Coord strafe = {0} ;
+    for(uint i = 0; i < 4; i++)
+        strafe = coordShift(strafe, i, 2*keyState(dirkey[i]));
+
+    player.pos = cfAdd(player.pos, cfRotateDeg(CCf(strafe), player.ang));
+    return player;
+}
+
+Player playerMoveKeys(Player player)
+{
+    player.ang = degReduce(player.ang + keyState(SDL_SCANCODE_D) - keyState(SDL_SCANCODE_A));
+    player.pos = cfAdd(
+        player.pos,
+        degMagToCf(player.ang, 2*(float)(keyState(SDL_SCANCODE_W) - keyState(SDL_SCANCODE_S)))
+    );
+    return player;
+}
+
+Wall* mapLoad(char *fileName)
+{
+    File *file = NULL;
+    assertExpr(fileName);
+    if((file = fopen(fileName, "rb")) == NULL){
+        printf("Couldn't open file \"%s\"\n", fileName);
+        return NULL;
+    }
+    uint len = 0;
+    fread(&len, sizeof(uint), 1, file);
+    if(feof(file) || len == 0){
+        printf("Error reading len of map in file \"%s\"\n", fileName);
+        fclose(file);
+        return NULL;
+    }
+    WallPacked *mapPacked = calloc(len, sizeof(WallPacked));
+    fread(mapPacked, sizeof(WallPacked), len, file);
+    if(fgetc(file) != EOF || !feof(file))
+        printf("Not at end of file after reading expected len (%u)\n", len);
+    Wall* map = mapUnpack(mapPacked, len);
+    free(mapPacked);
+    return map;
+}
+
+#endif /* end of include guard: WOLFENDOGE_H */
