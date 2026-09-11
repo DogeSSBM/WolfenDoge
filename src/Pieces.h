f2a17c750f41dd65128419cd84405cb556ac2b4c
DogeSSBM <dogessbm@gmail.com>
2026-09-11 19:19:29 -0400

    changed pieceNew() to call respective new obj functions when creating a new obj instead of always using objNew()

--- src/Pieces.h
+++ src/Pieces.h
@@ -308,10 +308,24 @@ MapPiece pieceNew(const NewPieceInfo pieceInfo, const Coordf a, const Coordf b)
 {
     assertExpr(pieceInfo.pieceType < M_ANY);
     MapPiece piece = {.type = pieceInfo.pieceType};
-    if(piece.type == M_SEG)
+    if(piece.type == M_SEG){
         piece.seg = segNew(pieceInfo.segType, a, b);
-    else
-        piece.obj = objNew(pieceInfo.objType, a);
+    }else{
+        switch(pieceInfo.objType){
+            case O_KEY:
+                piece.obj = keyNew(a, GREEN);
+                break;
+            case O_MOB:
+                piece.obj = mobNew(a, "./Assets/Doggo.png");
+                break;
+            case O_SPAWN:
+                piece.obj = spawnNew(a, 0);
+                break;
+            default:
+                panic("Unspecified ObjType");
+                break;
+        }
+    }
     return piece;
 }
 
