59de9241975fd5f7fa644863c91080d4de53b57f
DogeSSBM <dogessbm@gmail.com>
2026-09-11 21:35:47 -0400

    fixed script

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@ set -ex
 input="./src/main.c"
 output="./main.out"
 libraries="-lm -lSDL2 -lSDL2_ttf -lSDL2_image"
-flags="-std=c11 -Wall -Wextra -Wpedantic -Werror -flto -g"
+flags="-std=c11 -Wall -Wextra -Wpedantic -Werror -g"
 gcc $input  $flags $libraries -o $output
